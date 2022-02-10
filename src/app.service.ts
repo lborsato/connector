d218708afc9f060a1837bce3ce4d1730074e38a7
Larry Borsato <larry@larryborsato.com>
2022-02-10 14:17:14 -0500

    Fix display line.

--- src/app.service.ts
+++ src/app.service.ts
@@ -35,12 +35,13 @@ export class AppService {
   }
 
   getTransactions(
-      object: string | string[] | undefined,
-      object_id: string | string[] | undefined,
+    object: string | string[] | undefined,
+    object_id: string | string[] | undefined,
   ): string {
     // let html = '<h2>Last Three Transactions for ' + this.customer.id + ':</h2>';
     console.log('object: ', object);
-    let html = '<h2>Last Three Transactions for ${object} ${objectid}:</h2>';
+    let html =
+      '<h2>Last Three Transactions for ' + object + ' ' + object_id + ':</h2>';
     html += '<table width="100%">';
     html += '<tr><th>Date</th><th>Description</th><th>Amount</th></tr>';
     html += '<tr><th>01/30/2022</th><th>Nespresso</th><th>$25.88</th></tr>';
