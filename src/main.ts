08d52c40103d21cf2cd9571e4e9305a51a880ba7
Larry Borsato <larry@larryborsato.com>
2022-02-10 14:54:12 -0500

    Format dates again.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  DateFormatInterceptor,
-  DateFormat,
-} from 'nestjs-date-format-interceptor/lib/date';
+// import {
+//   DateFormatInterceptor,
+//   DateFormat,
+// } from 'nestjs-date-format-interceptor/lib/date';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalInterceptors(
-    new DateFormatInterceptor({ maxDeep: 6, format: DateFormat.toISOString }),
-  );
+  // app.useGlobalInterceptors(
+  //   new DateFormatInterceptor({ maxDeep: 6, format: DateFormat.toISOString }),
+  // );
   await app.listen(3000);
 }
 bootstrap();
