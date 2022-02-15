7efb1ae38691679e429067d2efbca57a5950e971
Larry Borsato <larry@larryborsato.com>
2022-02-15 15:34:13 -0500

    Remove swagger for now.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const options = new DocumentBuilder()
-    .setTitle('Connector example')
-    .setDescription('The Connector API description')
-    .setVersion('1.0')
-    .addTag('connector')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
   await app.listen(process.env.PORT);
