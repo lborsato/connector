e01d5b866464b8a0f4707f90cb3e482d685f5f34
Larry Borsato <larry@larryborsato.com>
2022-02-14 19:48:23 -0500

    Get url after app.listen().

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log(app.getUrl());
   await app.listen(process.env.PORT);
+  console.log(app.getUrl());
 }
 bootstrap();
