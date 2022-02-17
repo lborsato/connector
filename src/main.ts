99770d1649de739137268e3b59cfd12911d0710f
Larry Borsato <larry@larryborsato.com>
2022-02-16 19:51:09 -0500

    Swagger in again.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //     .setTitle(process.env.name)
-  //     .setDescription(process.env.name)
-  //     .setVersion(process.env.version)
-  //     .addTag('connector')
-  //     .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+      .setTitle(process.env.name)
+      .setDescription(process.env.name)
+      .setVersion(process.env.version)
+      .addTag('connector')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
   await app.listen(process.env.PORT);
